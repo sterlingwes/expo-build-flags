e0517736f02702174bcef04c658f85de02a8bfa9
Wes Johnson <wes@swj.io>
2024-09-23 20:29:09 -0400

    ci: make local pkg install more realistic

--- test/integration.sh
+++ test/integration.sh
@@ -10,9 +10,7 @@ CI=1 npx create-expo-app example
 
 echo "Installing library"
 cd example
-npm install --save-dev ../
-# ensure we don't carry over any dependencies via local install
-rm -rf node_modules/expo-build-flags/node_modules
+npm install --install-links --save-dev ../
 
 echo "copy over flag fixture"
 cp ../test/integration/default-flags.json ./flags.json
